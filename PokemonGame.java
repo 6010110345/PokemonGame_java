42043411c4823aa5ac084f83cb93b9c7493eea4a
6010110345 <6010110345@psu.ac.th>
2020-05-02 01:52:35 +0700

    commit on master

--- PokemonGame.java
+++ PokemonGame.java
@@ -1,7 +1,7 @@
 
 public class PokemonGame{
     public static void main(String[] arg){
-        System.out.println("Hello Pokemon Game ");
+        System.out.println("Welcome to Pokemon Game ");
         Trainer t = new Trainer();
         t.play();
         System.out.println("End Pokemon Game ");
